fa639d59f530c61f8f6b11b65d17a801ebd1baf1
ethanDe <ethand971@deltalearns.ca>
2022-01-13 14:07:23 -0800

    Almost done

--- x04_game.py
+++ x04_game.py
@@ -11,29 +11,29 @@ from x03_winner import *
 
 if __name__ == "__main__":
   pass
+while True:
+  x = playerChoice()
+  y = computerChoice()
+  z = playerWins(y,x)
 
-x = playerChoice()
-y = computerChoice()
-z = playerWins(y,x)
-
-if x == 0:
-  if y == 0:
-    print("tie")
-  elif y == 1:
-    print("win")
-  elif y ==2:
-    print("ssss")
-elif x == 1:
-  if y == 0:
-    print("win")
-  elif y == 1:
-    print("tie")
-  elif y == 2:
-    print("loss")
-elif x == 2:
-  if y == 0:
-    print("loss")
-  elif y== 1:
-    print("win")
-  elif y == 2:
-    print("tie")
+  if x == 0:
+    if y == 0:
+      print("tie")
+    elif y == 1:
+      print("win")
+    elif y ==2:
+      print("loss")
+  elif x == 1:
+    if y == 0:
+      print("win")
+    elif y == 1:
+      print("tie")
+    elif y == 2:
+      print("loss")
+  elif x == 2:
+    if y == 0:
+      print("loss")
+    elif y== 1:
+      print("win")
+    elif y == 2:
+      print("tie")
